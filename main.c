39d54cb4f5c6e0eb0f875c4b711c759c3ee28064
Efim Aksenov <Efimke54@gmail.com>
2019-12-17 13:07:22 +0700

    infix to prefix algorithm

--- main.c
+++ main.c
@@ -15,30 +15,9 @@ struct Node {
 struct Node *create_node(int value) {
     struct Node *new = (struct Node *) malloc(sizeof(struct Node));
     new->value = value;
+    new->link = NULL;
     return new;
 }
-
-//void delete_last(struct Node *node) {
-//    if (node->link == NULL) {
-//        free(node);
-//    } else {
-//        delete_last(node->link);
-//    }
-//}
-//
-//struct Node *get_last_node(struct Node *node) {
-//    if (node->link == NULL) {
-//        return node;
-//    } else {
-//        get_last_node(node->link);
-//    }
-//}
-//
-//void add_an_last_position(char value, struct Node *node) {
-//    if (node != NULL) {
-//        get_last_node(node)->value = value;
-//    }
-//}
 
 /*
  Stack block
@@ -49,6 +28,7 @@ struct Stack {
 
 struct Stack *create_stack() {
     struct Stack *new = (struct Stack *) malloc(sizeof(struct Stack));
+    new->top = NULL;
     return new;
 }
 
@@ -60,11 +40,13 @@ int pop(struct Stack *stack) {
     return value;
 }
 
-//char peek(struct Stack *stack) {
-//    if (stack->top != NULL) {
-//        return stack->top->value;
-//    }
-//}
+int is_empty(struct Stack *stack) {
+    return stack->top == NULL;
+}
+
+int peek(struct Stack *stack) {
+    return stack->top->value;
+}
 
 void push(struct Stack *stack, int value) {
     struct Node *new = create_node(value);
@@ -77,6 +59,7 @@ void push(struct Stack *stack, int value) {
 /*
  * -------------------------------------------------------
  */
+
 int is_operator(char value) {
     if (value == '^' || value == '*' || value == '/' || value == '+' || value == '-') {
         return 1;
@@ -95,6 +78,19 @@ int precedence(char value) {
     else
         return 0;
 }
+
+char *reverse_string(char *string) {
+    int len = (int) strlen(string);
+    char *new_string = (char *) malloc(sizeof(char) * len);
+    int i = len - 1, j = 0;
+    for (; i >= 0; i--, j++) {
+        new_string[j] = string[i];
+    }
+    free(string);
+    new_string[j] = '\0';
+    return new_string;
+}
+
 
 int evaluate_prefix(char *prefix) {
     struct Stack *stack = create_stack();
@@ -131,6 +127,49 @@ int evaluate_prefix(char *prefix) {
     return pop(stack);
 }
 
+
+char *infix_to_prefix(char *infix) {
+    char symb;
+    struct Stack *stack = create_stack();
+    int infix_len = (int) strlen(infix);
+    char *output = (char *) malloc(sizeof(char) * infix_len);
+    int output_index = 0;
+    int i;
+    for (i = infix_len - 1; i >= 0; i--) {
+        symb = infix[i];
+        if (isdigit(symb)) {
+            output[output_index] = symb;
+            output_index++;
+        } else if (symb == ')') {
+            push(stack, symb);
+        } else if (is_operator(symb)) {
+            if (is_empty(stack) || (char) peek(stack) == ')') {
+                push(stack, symb);
+            } else if (precedence((char) peek(stack)) <= precedence(symb)) {
+                push(stack, symb);
+            } else {
+                output[output_index] = (char) pop(stack);
+                output_index++;
+                push(stack, symb);
+            }
+        } else if (symb == '(') {
+            while (1) {
+                char operator = (char) pop(stack);
+                if (operator == ')') break;
+                output[output_index] = operator;
+                output_index++;
+            }
+        }
+    }
+    while (!is_empty(stack)) {
+        char operator = (char) pop(stack);
+        output[output_index] = operator;
+        output_index++;
+    }
+    output[output_index] = '\0';
+    return reverse_string(output);
+}
+
 /*
  * ------------------------
  */
@@ -144,4 +183,13 @@ int main() {
 
     char test_prefix_exp3[] = "+/*23-21*5-41";
     printf("Result: %d, expected %d \n", evaluate_prefix(test_prefix_exp3), 21);
+
+    char test_infix1[] = "1+2";
+    printf("Result: %s, expected %s \n", infix_to_prefix(test_infix1), "+12");
+
+    char test_infix2[] = "3*(1+1)";
+    printf("Result: %s, expected %s \n", infix_to_prefix(test_infix2), "*3+11");
+
+    char test_infix3[] = "2*3/(2-1)+5*(4-1)";
+    printf("Result: %s, expected %s \n", infix_to_prefix(test_infix3), "+/*23-21*5-41");
 }
